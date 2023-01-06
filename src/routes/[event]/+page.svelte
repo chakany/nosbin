8328447dbbdbf53bee3e7e5cd5852aa9eb6b322f
Jack Chakany <jacany@chaker.net>
2023-01-06 09:45:06 -0500

    markdown support

--- src/routes/[event]/+page.svelte
+++ src/routes/[event]/+page.svelte
@@ -2,9 +2,11 @@
     import { nostrInstance } from "$lib/store";
     import { page } from "$app/stores";
     import { HighlightAuto, LineNumbers } from "svelte-highlight";
+    import SvelteMarkdown from "svelte-markdown";
     import github from "svelte-highlight/styles/github-dark";
     let event;
     let hasData = false;
+    let content = ""
 
     // fetch
     async function fetch() {
@@ -21,13 +23,17 @@
   {@html github}
 </svelte:head>
 
-{#if !hasData}
-  <h2>Fetching...</h2>
-  {:else}
+{#if hasData}
   <h2>{event?.tags[0][1]}</h2>
   Posted by: {event.pubkey} <br />
   Posted on: {new Date(event.created_at * 1000)}
-  <HighlightAuto langtag={true} code={event.content} let:highlighted>
-    <LineNumbers {highlighted} hideBorder wrapLines />
-  </HighlightAuto>
+  {#if event?.tags[0][1].endsWith(".md")}
+    <SvelteMarkdown source={event.content} />
+    {:else}
+    <HighlightAuto code={event.content} let:highlighted>
+      <LineNumbers {highlighted} hideBorder wrapLines />
+    </HighlightAuto>
+  {/if}
+  {:else}
+  <h2>Fetching...</h2>
 {/if}
