042af9918ff08bfe2b9a5e35a23085a782e3deb7
Jack Chakany <jackchakany@outlook.com>
2023-02-12 23:38:33 -0500

    remove caret

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -239,10 +239,9 @@
 					>Made with ❤️ by <a class="underline" href="https://jacany.com">Jack Chakany</a
 					></small
 				>
-				<small class="my-auto"
-					>npub1chakany8dcz93clv4xgcudcvhnfhdyqutprq2yh72daydevv8zasmuhf02
-					></small
-				>
+				<small class="my-auto">
+					npub1chakany8dcz93clv4xgcudcvhnfhdyqutprq2yh72daydevv8zasmuhf02
+				</small>
 				<small class="my-auto"
 					>This project is published under the <a
 						href="https://github.com/jacany/nosbin/blob/master/LICENSE"
